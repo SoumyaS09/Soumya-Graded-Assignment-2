51017761ab3a8798e31adfdf5319b1438f033777
SoumyaS09 <shetty.soumya09@yahoo.in>
2021-12-31 23:49:43 +0530

    Create Model.java

---
+++ src/com/sorting/service/Model.java
@@ -0,0 +1,18 @@
+package com.sorting.service;
+
+public class Model {
+	private String noOfCompanies;
+	private double stockPrice;
+	 
+	 public Model(String noOfCompanies, double stockPrice,  boolean[] arr) {
+		 
+	 }
+
+	public String getNoOfCompanies() {
+		return noOfCompanies;
+	}
+
+	public double getStockPrice() {
+		return stockPrice;
+	}
+}
